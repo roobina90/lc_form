cfae48248c8ae821daed2d989ff31aaf6fef6b85
roobina90 <roobina1990@gmail.com>
2017-07-22 21:40:51 +0200

    validation fro fields

--- src/controls/Field.js
+++ src/controls/Field.js
@@ -5,35 +5,46 @@ import Error from './Error';
 
 
 class Field extends Component {
-  constructor(props) {
-    super(props);
-    this.handleChange = this.handleChange.bind(this);
-  }
+    constructor(props) {
+        super(props);
+        this.state = {
+            isValid: true,
+            isDirty: false
+        };
+        this.handleChange = this.handleChange.bind(this);
+        this.validate = this.validate.bind(this);
+        this.onBlur = this.onBlur.bind(this);
+    }
+    onBlur() {
+        this.setState({ isDirty: true });
+    }
+    validate() {
+        this.setState({ isValid: this.props.validation(this.props.value) });
+    }
+    handleChange(evt) {
+        this.validate();
+        this.props.onChange(this.props.name, evt.target.value);
+        evt.preventDefault();
+    }
+    render() {
+        return (
+            <div>
+                <label htmlFor={this.props.name}>{this.props.label}:</label>
+                <input onBlur={this.onBlur} type={this.props.type} name={this.props.name} onChange={this.handleChange} value={this.props.value} />
+                <Error message="" isShown={!this.state.isValid && this.state.isDirty} />
 
-  handleChange(evt) {
-      this.props.onChange(this.props.name, evt.target.value);
-      //this.setState({value: evt.target.value});
-      evt.preventDefault();
-  }
-  render() {
-    return (
-      <div>
-       <label htmlFor={this.props.name}>{this.props.label}:</label>
-        <input type={this.props.type} name={this.props.name} onChange={this.handleChange} value={this.props.value} />
-        <Error message="Tu jest walidacja" isShown={!this.props.validation(this.props.value)} />
-
-      </div>
-    );
-  }
+            </div>
+        );
+    }
 }
 
 Field.propTypes = {
-  name: PropTypes.string.isRequired,
-  label: PropTypes.string.isRequired,
-  type: PropTypes.string.isRequired,
-  value: PropTypes.string.isRequired,
-  onChange: PropTypes.func.isRequired,
-  validation: PropTypes.func.isRequired
+    name: PropTypes.string.isRequired,
+    label: PropTypes.string.isRequired,
+    type: PropTypes.string.isRequired,
+    value: PropTypes.string.isRequired,
+    onChange: PropTypes.func.isRequired,
+    validation: PropTypes.func.isRequired
 };
 
 export default Field;
